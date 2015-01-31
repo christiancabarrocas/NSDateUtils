22704a925fb11850a38acb386209539d75639c6d
Christian Cabarrocas <ccabarrocas@MacBook-Pro-de-Christian.local>
2015-01-31 16:32:23 +0100

    Initial Commit

---
+++ NSDateUtils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSDateUtils
+//
+//  Created by Christian Cabarrocas on 31/1/15.
+//  Copyright (c) 2015 ccs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
